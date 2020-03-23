a79e2f64a65c9d4f12e3c515252590160fa596fb
weiliya <viktoria.szabo.cv@gmail.com>
2020-03-23 15:59:42 +0100

    Chinese added

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import SignUpScreen from "./app/screens/signUp/SignUpScreen";
 export default function App() {
 
     return (
-            <SignUpSuccessScreen/>
+            <PasswordSentScreen/>
     );
 
 }
